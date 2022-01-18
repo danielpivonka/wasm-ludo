116dbdfed5f804ef69f21c0137a0c42d51fa4aa8
Michal Čížek <cizek@inqool.cz>
2022-01-18 10:48:23 +0100

    formatted and added structure folders

--- server/src/components/info/routes.rs
+++ server/src/components/info/routes.rs
@@ -3,7 +3,5 @@ use actix_web::web;
 use super::controller;
 
 pub fn attach_routes(config: &mut web::ServiceConfig) {
-  config.service(
-    web::scope("/info").service(controller::get_info)
-  );
+  config.service(web::scope("/info").service(controller::get_info));
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,20 @@
-use actix_web::{App, HttpServer, web, HttpResponse, middleware};
+use actix_web::{middleware, App, HttpServer};
 use env_logger::Env;
 
 mod components;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+  env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(|| {
-        App::new()
-        .wrap(middleware::Logger::default())
-        .configure(components::info::routes::attach_routes)
-        .route("/", web::get().to(|| HttpResponse::Ok().body("/ main")))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await?;
+  HttpServer::new(|| {
+    App::new()
+      .wrap(middleware::Logger::default())
+      .configure(components::info::routes::attach_routes)
+  })
+  .bind("127.0.0.1:8080")?
+  .run()
+  .await?;
 
   Ok(())
 }
