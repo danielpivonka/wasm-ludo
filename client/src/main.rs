89e0f6ee37ce492eb3c06d736402ba8f8f0141a0
Michal Čížek <cizek@inqool.cz>
2022-01-18 12:32:26 +0100

    client setup added

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,21 @@
+use yew::{function_component, html};
+
+mod components;
+
+use components::spinner::Spinner;
+
+#[function_component(App)]
+pub fn app() -> Html {
+  html! {
+    <div class="h-full grid place-items-center">
+      <div class="flex flex-col justify-center items-center">
+        <Spinner />
+        <h1 class="mt-4 font-semibold">{"Hello from the client, page is under construction"}</h1>
+      </div>
+    </div>
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  yew::start_app::<App>();
 }
