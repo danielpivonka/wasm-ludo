55820433ee76f72531cc647252e184308fec02af
Daniel Pivonka <xpivonk1@fi.muni.cz>
2022-01-28 03:47:42 +0100

    added common function for updating game state

--- server/src/components/game/database.rs
+++ server/src/components/game/database.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, post, web, HttpResponse};
-use mongodb::{bson::{oid::ObjectId, doc, bson, self}, results::UpdateResult};
+use mongodb::{bson::{oid::ObjectId, doc, bson, self, Document}, results::UpdateResult};
 
 use crate::{
   models::{game::Game, player::Player},
@@ -19,20 +19,9 @@ pub async fn create_game(data: web::Data<WebAppData>,creating_player:Player)->Re
 }
 
 pub async fn add_player(data: web::Data<WebAppData>,game_id :&str ,new_player:Player)->Result<UpdateResult,Box<dyn std::error::Error>>{
-  let db = &data.lock().await.db;
-  let game_collection = db.collection::<Game>("games");
-  let oid = match ObjectId::parse_str(game_id) {
-    Ok(res) => res,
-    Err(err) => return Err(Box::new(err)),
-  };
-  let filter = doc! { "_id" : oid };
   let serialized_player = bson::to_bson(&new_player)?;
   let update =  doc! { "$push": { "players": serialized_player } };
-  let res = game_collection.update_one(filter,update,None).await;
-     match res {
-      Ok(result) => Ok(result),
-      Err(e) => Err(Box::new(e)),
-    }
+  return update_game(data,game_id,update).await;
 }
 pub async fn find_game(data: web::Data<WebAppData>,game_id :&str) -> Result<Option<Game>,Box<dyn std::error::Error>>{
   let db = &data.lock().await.db;
@@ -47,4 +36,18 @@ pub async fn find_game(data: web::Data<WebAppData>,game_id :&str) -> Result<Opti
     Ok(result) => Ok(result),
     Err(e) => Err(Box::new(e)),
   }
+}
+async fn update_game(data: web::Data<WebAppData>,game_id :&str,update:Document)->Result<UpdateResult,Box<dyn std::error::Error>>{
+  let db = &data.lock().await.db;
+  let game_collection = db.collection::<Game>("games");
+  let oid = match ObjectId::parse_str(game_id) {
+    Ok(res) => res,
+    Err(err) => return Err(Box::new(err)),
+  };
+  let filter = doc! { "_id" : oid };
+  let res = game_collection.update_one(filter,update,None).await;
+     match res {
+      Ok(result) => Ok(result),
+      Err(e) => Err(Box::new(e)),
+    }
 }
