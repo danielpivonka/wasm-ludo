6365c0be5b93ffb96fdaedebf0cf122d626d1e4c
Daniel Pivonka <xpivonk1@fi.muni.cz>
2022-01-28 03:57:49 +0100

    modified function for updating

--- server/src/components/game/database.rs
+++ server/src/components/game/database.rs
@@ -21,7 +21,12 @@ pub async fn create_game(data: web::Data<WebAppData>,creating_player:Player)->Re
 pub async fn add_player(data: web::Data<WebAppData>,game_id :&str ,new_player:Player)->Result<UpdateResult,Box<dyn std::error::Error>>{
   let serialized_player = bson::to_bson(&new_player)?;
   let update =  doc! { "$push": { "players": serialized_player } };
-  return update_game(data,game_id,update).await;
+  let oid = match ObjectId::parse_str(game_id) {
+    Ok(res) => res,
+    Err(err) => return Err(Box::new(err)),
+  };
+  let filter = doc! { "_id" : oid };
+  return update_game(data,filter,update).await;
 }
 pub async fn find_game(data: web::Data<WebAppData>,game_id :&str) -> Result<Option<Game>,Box<dyn std::error::Error>>{
   let db = &data.lock().await.db;
@@ -41,16 +46,16 @@ pub async fn update_board(data: web::Data<WebAppData>,game_id :&str ,fields:Vec<
   
   let serialized_fields = bson::to_bson(&fields)?;
   let update =  doc! { "fields": serialized_fields };
-  return update_game(data,game_id,update).await;
-}
-async fn update_game(data: web::Data<WebAppData>,game_id :&str,update:Document)->Result<UpdateResult,Box<dyn std::error::Error>>{
-  let db = &data.lock().await.db;
-  let game_collection = db.collection::<Game>("games");
   let oid = match ObjectId::parse_str(game_id) {
     Ok(res) => res,
     Err(err) => return Err(Box::new(err)),
   };
   let filter = doc! { "_id" : oid };
+  return update_game(data,filter, update).await;
+}
+async fn update_game(data: web::Data<WebAppData>,filter:Document,update:Document)->Result<UpdateResult,Box<dyn std::error::Error>>{
+  let db = &data.lock().await.db;
+  let game_collection = db.collection::<Game>("games");
   let res = game_collection.update_one(filter,update,None).await;
      match res {
       Ok(result) => Ok(result),
